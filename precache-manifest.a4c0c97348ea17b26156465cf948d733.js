332b759bd593a8a17c8ee2b43bc2d8ffae735519
Kushagra <kushagraa110@gmail.com>
2022-04-25 03:50:36 +0530

    Updates

---
+++ precache-manifest.a4c0c97348ea17b26156465cf948d733.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c14456f52180b5de9850e9062e2d00",
+    "url": "/OnlyAds/index.html"
+  },
+  {
+    "revision": "85cb735a9e67e4794408",
+    "url": "/OnlyAds/static/css/2.0bfa1f85.chunk.css"
+  },
+  {
+    "revision": "f6f43ec3299cb5978fbd",
+    "url": "/OnlyAds/static/css/main.e5966b10.chunk.css"
+  },
+  {
+    "revision": "85cb735a9e67e4794408",
+    "url": "/OnlyAds/static/js/2.c1340259.chunk.js"
+  },
+  {
+    "revision": "c16c00b73717fd5c7d8336fe4c7a6157",
+    "url": "/OnlyAds/static/js/2.c1340259.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6f43ec3299cb5978fbd",
+    "url": "/OnlyAds/static/js/main.730eaae7.chunk.js"
+  },
+  {
+    "revision": "9b59ba190cad6e43e18e",
+    "url": "/OnlyAds/static/js/runtime-main.cb3dfe8b.js"
+  },
+  {
+    "revision": "847f6e8171ee5670ca9e686a14fd8325",
+    "url": "/OnlyAds/static/media/OnlyAds1.847f6e81.ico"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/OnlyAds/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "53b0b230ba1bc79b0f62b6addb8174ff",
+    "url": "/OnlyAds/static/media/barista.53b0b230.jpg"
+  },
+  {
+    "revision": "e69be2c082cf6c299934b0003258470e",
+    "url": "/OnlyAds/static/media/coffee-paper-cup.e69be2c0.jpg"
+  },
+  {
+    "revision": "afa13c8564fdc9efad4d2e738c71602a",
+    "url": "/OnlyAds/static/media/huge_cup.afa13c85.jpg"
+  },
+  {
+    "revision": "2b37885ef94a0fefa93208f259cc5b8f",
+    "url": "/OnlyAds/static/media/krispy.2b37885e.jpg"
+  },
+  {
+    "revision": "ce166a9d232d50e25d0f9b051bf41cfa",
+    "url": "/OnlyAds/static/media/loginbg1.ce166a9d.jpg"
+  },
+  {
+    "revision": "efe8c90f62e9a3ef3f9467d86fd61d57",
+    "url": "/OnlyAds/static/media/logo.efe8c90f.png"
+  },
+  {
+    "revision": "07ce6d2fb8a18ac166439bde78c78768",
+    "url": "/OnlyAds/static/media/mock1.07ce6d2f.png"
+  },
+  {
+    "revision": "060a0233517a8e63b0bf081dc24e4a9f",
+    "url": "/OnlyAds/static/media/mock2.060a0233.png"
+  },
+  {
+    "revision": "28b52cf0c78d544fe93cc7ea4420d0ab",
+    "url": "/OnlyAds/static/media/panda_cup.28b52cf0.jpg"
+  },
+  {
+    "revision": "4143d240af520e4a8b565060c6e6277b",
+    "url": "/OnlyAds/static/media/services.4143d240.jpg"
+  },
+  {
+    "revision": "a6e36203780084d9a4e7118439521732",
+    "url": "/OnlyAds/static/media/small_cup.a6e36203.jpg"
+  },
+  {
+    "revision": "34b8a275352b37d79c67b694f2a296e6",
+    "url": "/OnlyAds/static/media/tea.34b8a275.jpg"
+  }
+]);
